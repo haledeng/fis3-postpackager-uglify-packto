793856846fc1867d1ec95ac5181044df49db9db8
helondeng <dhlwust@163.com>
2016-08-04 16:05:24 +0800

    add include option

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ fis 配置uglify-js时，一般配置`'**.js'`，压缩慢。
 
 ### 功能
 只压缩`ret.pkg`，即产出的打包文件。
+
+### 配置
+include: ret.pkg外需要压缩的文件
+```
+fis.mathc('::package', {
+	postpackager: fis.plugin('uglify-packto', {
+		include: [filesubpath] 
+	})
+})
+```
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /*
-* 只要所pkg中打包的文件, 提高压缩速度
-*/
+ * 只要所pkg中打包的文件, 提高压缩速度
+ */
 var fs = require('fs');
 var UglifyJS = require('uglify-js');
+var toString = Object.prototype.toString;
 
+var uglify = function(content) {
+	return UglifyJS.minify(content, {
+		fromString: true
+	}).code;
+};
 
 module.exports = function(ret, conf, setting, opts) {
 	var pkg = ret.pkg,
@@ -17,9 +23,21 @@ module.exports = function(ret, conf, setting, opts) {
 			return;
 		}
 		content = file.getContent();
-		content = UglifyJS.minify(content, {
-			fromString: true
-		}).code;
+		content = uglify(content);
 		file.setContent(content);
 	});
+	var include = setting.include || [];
+	ids = Object.keys(ret.ids);
+
+	if (include && toString.call(include) === '[object Array]') {
+		var subpaths = [];
+		ids.forEach(function(id) {
+			if (~include.indexOf(ret.ids[id].subpath)) {
+				file = ret.ids[id];
+				content = file.getContent();
+				content = uglify(content);
+				file.setContent(content);
+			}
+		});
+	}
 };
